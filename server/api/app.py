502d023ceb8f253d58e28c7953808297fe80537b
Rucha Ambaliya <Rambaliyaforall@gmail.com>
2026-01-05 23:09:30 +0530

    Modify app.py

--- server/api/app.py
+++ server/api/app.py
@@ -1,10 +1,10 @@
 import os
 import sys
 # Add parent directory to Python path to allow Server module import
-# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from api import create_app
 # Initialize Flask app
 app = create_app()
 # start server
-# if __name__ == "__main__":
-#     app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
